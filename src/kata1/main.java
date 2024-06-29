862e6f7acb351b0e1f9c6733c3d693e7d7c7d8c1
Lsanara <nebeproyects@gmail.com>
2024-06-30 00:01:30 +0100

    Versión final del código con su correspodiente main

--- src/kata1/main.java
+++ src/kata1/main.java
@@ -4,7 +4,7 @@ import  java.time.LocalDate;
 
 public class main {
     public static void main(String[] args) {
-        LocalDate birth = LocalDate.parse("2002-03-03");
+        LocalDate birth = LocalDate.parse("2003-03-03");
         person laura = new person("Laura", birth);
         System.out.println(laura.getName() + " tiene " + laura.getAge() + " años");
     }
