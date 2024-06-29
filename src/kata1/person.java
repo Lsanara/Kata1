f0bcbe799faf29effd1711df8726aa71dbb7f29a
Lsanara <nebeproyects@gmail.com>
2024-06-29 23:51:12 +0100

    Creación de la clase person y su constructor

---
+++ src/kata1/person.java
@@ -0,0 +1,15 @@
+package kata1;
+import java.time.LocalDate;
+import java.time.Period;
+
+
+public class person {
+    private final String name;
+    private final LocalDate birthday;
+
+    public person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+}
